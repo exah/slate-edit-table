66241e9e7e170eb7745297f6143be9e042e7f5a6
John Grishin <r@exah.ru>
2017-10-17 15:50:23 +0300

    :arrow_up: Upgrade Slate 0.24 → 0.25

--- tests/schema-rows-require-same-columns-counts/transform.js
+++ tests/schema-rows-require-same-columns-counts/transform.js
@@ -2,6 +2,5 @@ const Slate = require('slate')
 
 module.exports = function (plugin, state) {
   const schema = new Slate.Schema(plugin.schema)
-  return state.change()
-    .normalize(schema)
+  return state.change().normalize(schema)
 }
